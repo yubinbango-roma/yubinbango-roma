67da462113058195eb62f34b6354411200a97677
mikami <mikami@tecpresso.co.jp>
2019-07-10 17:29:50 +0900

    WIP for roma

--- test/protractor.conf.js
+++
@@ -1,9 +0,0 @@
-exports.config = {
-  seleniumAddress: 'http://localhost:4444/wd/hub',
-  framework: 'jasmine',
-
-  jasmineNodeOpts: {
-    showColors: true,
-    defaultTimeoutInterval: 30000
-  }
-};
